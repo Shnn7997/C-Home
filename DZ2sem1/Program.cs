54f1807a7febaf4604a153627de0c88fd5bf4485
Nikita Shulgin <shnn7997@gmail.com>
2022-11-28 22:02:47 +0700

    Zadacha2Sem1

---
+++ DZ2sem1/Program.cs
@@ -0,0 +1,7 @@
+﻿using System;
+Console.Write("Введите первое число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2) Console.WriteLine($"Число {number1} больше числа {number2}");
+else Console.WriteLine($"Число {number2} больше числа {number1}");
