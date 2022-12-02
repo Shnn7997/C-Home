b08c0943a1555128980fe9a081d5909f690ee7bd
Nikita Shulgin <shnn7997@gmail.com>
2022-12-02 14:07:33 +0700

    Zadacha8Sem1

---
+++ DZ8Sem1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+
+int Number = Convert.ToInt32(Console.ReadLine());
+int CurentN = 1;
+
+while (CurentN <= Number)
+{
+    if(CurentN % 2 == 0) Console.Write($"{CurentN}, ");
+    //Console.Write(", ");
+    CurentN++;
+}
+Console.WriteLine("End");
