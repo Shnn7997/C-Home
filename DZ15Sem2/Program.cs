ca8f4668b2979c44e91f0b7cbdf10a0635371213
Nikita Shulgin <shnn7997@gmail.com>
2022-12-02 16:06:07 +0700

    DZ15Sem2

---
+++ DZ15Sem2/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+using System;
+using static System.Console;
+
+
+Write("Введите номер дня недели: ");
+int dayN = Convert.ToInt32(ReadLine());
+
+{
+    if (dayN == 6 || dayN == 7)
+    {
+        WriteLine($"{dayN}: этот день выходной");
+    }
+    else if (dayN < 1 || dayN > 7)
+    {
+        WriteLine($"{dayN}: нет такого дня недели");
+    }
+    else WriteLine($"{dayN} день не выходной");
+}
+
