e734c992849e48d7f5f2204ea3ee43e3bc18f88b
Nikita Shulgin <shnn7997@gmail.com>
2022-12-02 14:44:29 +0700

    DZ4Sem1

---
+++ DZ4Sem1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static System.Console;
+//using System.Collections.Generic;
+//using System.Text;
+
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+namespace ConsoleApplicationMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            WriteLine("Введите число а");
+            double a = Convert.ToDouble(ReadLine());
+            WriteLine("Введите число b");
+            double b = Convert.ToDouble(ReadLine());
+            WriteLine("Введите число c");
+            double c = Convert.ToDouble(ReadLine());
+            double max;
+
+            max = Math.Max(a, Math.Max(b, c));
+
+            WriteLine($"Число {max} - максимальное из 3х введенных чисел");
+
+            ReadLine();
+        }
+    }
+}
