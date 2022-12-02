d633f3b703f2b8189613f88293051216a158b5e7
Nikita Shulgin <shnn7997@gmail.com>
2022-12-02 15:59:56 +0700

    DZ10Sem2

---
+++ DZ10Sem2/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+using System;
+using static System.Console;
+
+int number = new Random().Next(100, 1000);
+WriteLine(number);
+while (number >= 100) number /= 10;
+int SecondDigit = number % 10;
+WriteLine(SecondDigit);
+ReadKey();
+
