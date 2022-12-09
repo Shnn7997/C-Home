8af2e91c7c069e2068f6469d9277fc6040692833
Nikita Shulgin <shnn7997@gmail.com>
2022-12-09 13:43:07 +0700

    DZ27sem4

---
+++ DZ27Sem4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Console;
+
+WriteLine("Введите число");
+WriteLine($"Сумма цифр  в числе = {DigitSum(Convert.ToInt32(ReadLine()))}");
+
+
+
+
+
+int DigitSum(int n)
+{
+    int sum = 0;
+    while (n != 0)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    return sum;
+}
