4ab50ecba77c2322f9f59d1d49d3967b063dbc7a
Nikita Shulgin <shnn7997@gmail.com>
2022-12-09 14:18:16 +0700

    DZ29sem4

--- DZ29Sem4/Program.cs
+++ DZ29Sem4/Program.cs
@@ -6,3 +6,19 @@ using getranarray;
 
 int[] array = Gra.GetRandomArray(8);
 WriteLine(String.Join(",", array));
+
+namespace getranarray
+{
+    public class Gra
+    {
+        public static int[] GetRandomArray(int size)
+        {
+            int[] result = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                result[i]=new Random().Next(8);
+            }
+            return result;
+        }
+    }
+}
