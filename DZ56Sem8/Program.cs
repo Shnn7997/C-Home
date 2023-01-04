2941b3b16b0fdab30727d45c30a5baba3555f98f
Nikita Shulgin <shnn7997@gmail.com>
2023-01-04 17:20:14 +0700

    DZ56Sem8

---
+++ DZ56Sem8/Program.cs
@@ -0,0 +1,69 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу,
+// которая будет находить строку с наименьшей суммой элементов.
+
+
+using System;
+using static System.Console;
+
+
+
+Write("Введите размер матрицы через пробел ");
+string[] sizeS = ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+int rows = int.Parse(sizeS[0]);
+int columns = int.Parse(sizeS[1]);
+int[,] ar = GetRandomArray(rows, columns);
+PrintArray(ar);
+int minSumLine = 0;
+int sumLine = SumLineElements(ar, 0);
+for (int i = 1; i < ar.GetLength(0); i++)
+{
+    int tempSumLine = SumLineElements(ar, i);
+    if (sumLine > tempSumLine)
+    {
+        sumLine = tempSumLine;
+        minSumLine = i;
+    }
+}
+
+WriteLine($"\n{minSumLine + 1} - строкa с наименьшей суммой элементов ({sumLine}) ");
+
+
+int SumLineElements(int[,] array, int i)
+{
+    int sumLine = array[i, 0];
+    for (int j = 1; j < array.GetLength(1); j++)
+    {
+        sumLine += array[i, j];
+    }
+    return sumLine;
+}
+
+
+
+
+int[,] GetRandomArray(int row, int column)
+{
+    int[,] result = new int[row, column];
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i, j] = new Random().Next(-100, 100);
+        }
+    }
+    return result;
+}
+
+
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Write($"{array[i, j]} ");
+        }
+        WriteLine();
+    }
+}
