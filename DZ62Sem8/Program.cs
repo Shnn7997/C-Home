30afb761bf63bc10b3bb5cdf6b8831ea345b958c
Nikita Shulgin <shnn7997@gmail.com>
2023-01-05 14:34:00 +0700

    DZ62Sem8

---
+++ DZ62Sem8/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 62: Заполните спирально массив 4 на 4.
+
+using System;
+using static System.Console;
+
+
+int n = 4;
+int[,] sqArr = new int[n, n];
+
+int temp = 1;
+int i = 0;
+int j = 0;
+
+while (temp <= sqArr.GetLength(0) * sqArr.GetLength(1))
+{
+    sqArr[i, j] = temp;
+    temp++;
+    if (i <= j + 1 && i + j < sqArr.GetLength(1) - 1)
+        j++;
+    else if (i < j && i + j >= sqArr.GetLength(0) - 1)
+        i++;
+    else if (i >= j && i + j > sqArr.GetLength(1) - 1)
+        j--;
+    else
+        i--;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Write($"{array[i, j]} ");
+        }
+        WriteLine();
+    }
+}
+
+PrintArray(sqArr);
