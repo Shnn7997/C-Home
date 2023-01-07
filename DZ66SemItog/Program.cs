32e096a60dc20d522bb7fcbf53d00bb68f6890ca
Nikita Shulgin <shnn7997@gmail.com>
2023-01-07 19:01:26 +0700

    DZ66SemItog

---
+++ DZ66SemItog/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+
+
+WriteLine("Введите число a: ");
+int a = Convert.ToInt32(ReadLine());
+WriteLine("Введите число b: ");
+int b = Convert.ToInt32(ReadLine());
+WriteLine($"Сумма элементов от {a} до {b} = {Sum(a, b)}");
+
+int Sum(int a, int b)
+{
+    if (a == b)
+        return b;
+    return b + Sum(a, b - 1);
+}
